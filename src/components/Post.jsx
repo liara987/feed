41feba35ed387ea318145419b509d562483ce9a1
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-26 14:21:45 -0300

    feature: Formate date and adding comments on click

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,52 +1,77 @@
-import { Avatar } from './Avatar';
-import { Comment } from './Comment';
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const [comments, setComments] = useState([1, 2]);
+
+  const datePublishedFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
+
+  const publishedRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  function heandleNewComments() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="http://github.com/liara987.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Liara</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="25 de abril às 21:10h" dateTime="2023-04-04">Publicado a 1h</time>
+        <time
+          title={datePublishedFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a>
-          <a href="">#nlw</a>
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map((item) => {
+          if (item.type === "paragraph") {
+            return <p>{item.content}</p>;
+          } else if (item.type === "link") {
+            return (
+              <p>
+                <a href={item.content}>{item.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={heandleNewComments} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea
-            placeholder="Deixe um comentario"
-        />
+        <textarea placeholder="Deixe um comentario" />
+
         <footer>
-            <button type="submit">Comentar</button>
+          <button type="submit">Comentar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
