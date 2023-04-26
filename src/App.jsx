3ab128cc6e67c916aadd2e57bdbc19afd0c6821e
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-25 21:55:35 -0300

    feature: Create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { Header } from "./components/Header";
-import './global.css'
+import { Sidebar } from "./components/Sidebar";
+
+import style from "./App.module.css";
+import "./global.css";
 
 function App() {
   return (
     <>
       <Header />
+      <div className={style.wrapper}>
+        <Sidebar />
+
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio id
+          consectetur voluptate quo enim accusantium eius? Tenetur natus
+          perferendis ea necessitatibus praesentium quisquam. Corporis
+          distinctio iusto voluptatem veniam placeat voluptatum?
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilLine } from "@phosphor-icons/react";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="http://github.com/liara987.png" />
+        <strong>Liara</strong>
+        <span>Web developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
