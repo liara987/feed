4448cdd21ef792ab900fc59112ac0e78c46f6e57
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-26 00:25:23 -0300

    feature: Apply avatar componet

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from "./Post.module.css";
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="http://github.com/liara987.png" />
+          <Avatar src="http://github.com/liara987.png" />
           <div className={styles.authorInfo}>
             <strong>Liara</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from "@phosphor-icons/react";
 import styles from "./Sidebar.module.css";
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -8,8 +9,9 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
-      <div className={styles.profile}>
-        <img className={styles.avatar} src="http://github.com/liara987.png" />
+      <div className={styles.profile}>        
+        <Avatar src="http://github.com/liara987.png"/>
+        
         <strong>Liara</strong>
         <span>Web developer</span>
       </div>
