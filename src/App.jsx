0954178aced5d94c2c25f7937c210e7a37702fd7
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-26 14:20:22 -0300

    feture: Add data to variable

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from "./components/Post";
 import style from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/liara987.png',
+      name: 'Liara',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Hi everyone 👋'},
+      {type: 'paragraph', content: 'Just posted a new project to my profile, checkitout'},
+      {type: 'link', content: 'https://github.com/liara987/feed'},
+    ],
+    publishedAt: new Date('2023-04-25 21:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://github.com/andressamonteirocarvalho.png',
+      name: 'Andressa Monteiro Carvalho',
+      role: 'Fullstack Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Hello 🥰'},
+      {type: 'paragraph', content: 'I update my github profile, it would be great if you could take a look 🤗'},
+      {type: 'link', content: 'https://github.com/andressamonteirocarvalho/andressamonteirocarvalho'},
+    ],
+    publishedAt: new Date('2023-04-26 13:00:00')
+  },
+]
+
 function App() {
   return (
     <>
@@ -13,8 +44,16 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(porst => {
+            return (
+              <Post
+                key={porst.id}
+                author={porst.author}
+                content={porst.content}
+                publishedAt={porst.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
